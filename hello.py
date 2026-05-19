ced3bab887125376cfa9edc0d2864f284e058b17
Oumi-code1 <oumaimagazi5@gmail.com>
2026-05-19 15:22:03 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,25 @@
-#hello.py
-print("="*40)
-print("Bienvenue dans mon premier job Jenkins!")
-print("="*40)
+# hello.py (version sans input)
+import sys
 
-nom=input("Quel est ton nom?")
-print(f"Bonjour {nom}, ton job Jenkins a reussi!")
+print("=" * 40)
+print("Bienvenue dans mon premier job Jenkins !")
+print("=" * 40)
 
-#petit calcul
-a=10
-b=5
-print(f"{a}+{b}={a+b}")
-printf(f"{a}-{b}={a-b}")
+if len(sys.argv) > 1:
+    nom = sys.argv[1]
+else:
+    nom = "Étudiant Jenkins"
+
+print(f"Bonjour {nom}, ton job Jenkins a réussi !")
+
+# Petit calcul
+a = 10
+b = 5
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+
+# Simuler un test
+assert a + b == 15, "Le test a échoué !"
+print("✅ Tous les tests passent avec succès")
 
 
