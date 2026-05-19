d51afa84ec349f80180d28999d76b24be7c3660a
Oumi-code1 <oumaimagazi5@gmail.com>
2026-05-19 12:17:56 +0100

    "Add hello.py script"

---
+++ hello.py
@@ -0,0 +1,15 @@
+#hello.py
+print("="*40)
+print("Bienvenue dans mon premier job Jenkins!")
+print("="*40)
+
+nom=input("Quel est ton nom?")
+print(f"Bonjour {nom}, ton job Jenkins a reussi!")
+
+#petit calcul
+a=10
+b=5
+print(f"{a}+{b}={a+b}")
+printf(f"{a}-{b}={a-b}")
+
+
